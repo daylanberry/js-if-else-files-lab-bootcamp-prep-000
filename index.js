ead3583abea0c10d3ed6a5c1964cd5386e212dee
proactive-point-1823 <daylanberry@gmail.com>
2019-04-21 23:06:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var name = "Joe"
 var height = "74"
+if (name === 'Susan'){
+  console.log("This name is correct")
+}
 
+console.log('Expected the name to be Susan')
 
 // Don't worry about this
 module.exports = { name, height
